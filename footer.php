0fdc028299c95e29bcf22901140b03aa5d3ce745
Emile Kott <emilekott@gmail.com>
2013-02-18 09:38:04 +0000

    added privacy and cookie policy

--- footer.php
+++ footer.php
@@ -5,10 +5,10 @@
 	
 		<div id="footer-wrapper" class="container_12">
 		
-			<div id="footerContent" class="grid_12">
+			<div id="footerContent" class="grid_12">v
                             <div class="grid_9 alpha">
 				<p>
-					Copyright &copy; <?php echo date("Y");?> &bull; <a href="<?php echo get_option('home'); ?>/">Napier Partnership Limited</a> &bull; All Rights Reserved
+					Copyright &copy; <?php echo date("Y");?> &bull; <a href="<?php echo get_option('home'); ?>/">Napier Partnership Limited</a> &bull; All Rights Reserved &bull; <a href="<?php echo get_option('home'); ?>/go/privacy-and-cookie-information">Privacy &amp; Cookie Policy</a>
 					<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
 				</p>
                             </div>
