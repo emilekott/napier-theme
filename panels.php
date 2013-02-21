38a2469dad1bde662fd7efeeaa1bfd80cf568499
Emile Kott <emilekott@gmail.com>
2013-02-21 12:28:17 +0000

    panel link changed

--- panels.php
+++ panels.php
@@ -3,15 +3,15 @@
         <div class="panel-group">
             <a href="?p=4569" class="pane triple" id="pane1"></a>
             <a href="?p=5795" class="hover-panel pane single" id="pane5" ></a>
-            <a href="?p=3063=" class="pane single" id="pane6"></a>
-            <a href="#" class="pane single" id="pane7"></a>
+            <a href="?p=3063" class="pane single" id="pane6"></a>
+            <span class="pane single" id="pane7"></span>
         </div>
         
         <div class="pane" id="pane2"></div>
         
         <div class="panel-group">
             <a href="?p=3342" class="hover-panel pane single" id="pane3"></a>    
-            <a href="pdfs/ideas-4-b2b-lead-generation.pdf" class="hover-panel pane double" id="pane4"></a><div class="clear"></div>      
+            <a href="pdfs/ideas-for-b2b-lead-generation.pdf" target="_blank" class="hover-panel pane double" id="pane4"></a><div class="clear"></div>      
             <a href="?p=3346" class="hover-panel pane single" id="pane8"></a>
             <a href="?p=3348" class="hover-panel pane single" id="pane9"></a>
             <a href="?p=3063" class="hover-panel pane single" id="pane10"></a>
