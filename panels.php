7a2801ec0f008d5ea040ec4d38941debf96f4acc
Emile Kott <emilekott@gmail.com>
2013-02-21 10:44:58 +0000

    final changes and links

--- panels.php
+++ panels.php
@@ -1,20 +1,20 @@
 <div class="grid_12">
     <div id="panels" class="no-js">
         <div class="panel-group">
-            <a href="#" class="pane triple" id="pane1"></a>
-            <a href="#" class="hover-panel pane single" id="pane5" ></a>
-            <a href="#" class="pane single" id="pane6"></a>
+            <a href="?p=4569" class="pane triple" id="pane1"></a>
+            <a href="?p=5795" class="hover-panel pane single" id="pane5" ></a>
+            <a href="?p=3063=" class="pane single" id="pane6"></a>
             <a href="#" class="pane single" id="pane7"></a>
         </div>
         
         <div class="pane" id="pane2"></div>
         
         <div class="panel-group">
-            <a href="#" class="pane single" id="pane3"></a>    
-            <a href="#" class="hover-panel pane double" id="pane4"></a><div class="clear"></div>      
-            <a href="#" class="hover-panel pane single" id="pane8"></a>
-            <a href="#" class="pane single" id="pane9"></a>
-            <a href="#" class="hover-panel pane single" id="pane10"></a>
+            <a href="?p=3342" class="hover-panel pane single" id="pane3"></a>    
+            <a href="pdfs/ideas-4-b2b-lead-generation.pdf" class="hover-panel pane double" id="pane4"></a><div class="clear"></div>      
+            <a href="?p=3346" class="hover-panel pane single" id="pane8"></a>
+            <a href="?p=3348" class="hover-panel pane single" id="pane9"></a>
+            <a href="?p=3063" class="hover-panel pane single" id="pane10"></a>
         </div>
         
         <div class="clear"></div>
